7afd480c4f0bd22ae486040a12b1d7adc732d2dc
Top tinker 50/50 <tropin@mintmail.ru>
2021-03-27 00:39:33 +0500

    Profile

--- restcorporate_portal/Controllers/WorkerController.cs
+++ restcorporate_portal/Controllers/WorkerController.cs
@@ -7,19 +7,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using restcorporate_portal.Exceptions;
-using restcorporate_portal.Models;
+using Models = restcorporate_portal.Models;
 using restcorporate_portal.ResponseModels;
+using restcorporate_portal.Utils;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace restcorporate_portal.Controllers
 {
+    static class WorkersErrorsMessages
+    {
+        public const string WorkerNotFound = "WORKER_NOT_FOUND";
+    }
+
     [Route("api/workers")]
     [ApiController]
     public class WorkerController : ControllerBase
     {
-        private readonly corporateContext _context;
+        private readonly Models.corporateContext _context;
 
-        public WorkerController(corporateContext context)
+        public WorkerController(Models.corporateContext context)
         {
             _context = context;
         }
@@ -39,84 +45,135 @@ namespace restcorporate_portal.Controllers
                 .ToListAsync();
 
 
-            return Ok(workers.Select(x => ResponseWorkerList.FromApiWorker(x)).ToList());
+            return Ok(workers.Select(x => ResponseWorkerList.FromApiWorker(x, avatar: _getAvatar(x.AvatarUrl))).ToList());
         }
 
         // GET: api/workers/5
+        [SwaggerOperation(
+            Summary = "Детализация по работяге",
+            Tags = new string[] { "Работяги" }
+        )]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Ошибка", type: typeof(ExceptionInfo))]
+        [SwaggerResponse(StatusCodes.Status200OK, "Успешно", type: typeof(ResponseWorkerList))]
+        [Authorize(AuthenticationSchemes = "Bearer")]
         [HttpGet("{id}")]
-        public async Task<ActionResult<Worker>> GetWorker(int id)
+        public async Task<ActionResult<ResponseWorkerList>> GetWorker(int id)
         {
-            var worker = await _context.Workers.FindAsync(id);
+            var worker = await _context.Workers
+                .Include(x => x.Speciality)
+                .SingleOrDefaultAsync(x => x.Id == id);
 
             if (worker == null)
             {
-                return NotFound();
+                return NotFound(new ExceptionInfo {
+                    Message = WorkersErrorsMessages.WorkerNotFound,
+                    Description = "Работяга не найден"
+                });
             }
 
-            return worker;
+            return Ok(ResponseWorkerList.FromApiWorker(worker, avatar: _getAvatar(worker.AvatarUrl)));
         }
 
-        // PUT: api/Worker/5
+        // GET: api/workers/profile/
+        [SwaggerOperation(
+            Summary = "Получить профиль работяги",
+            Tags = new string[] { "Работяги" }
+        )]
+        [SwaggerResponse(StatusCodes.Status200OK, "Успешно", type: typeof(ResponseWorkerList))]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        [HttpGet("profile/")]
+        public async Task<ActionResult<ResponseWorkerList>> GetMe()
+        {
+            var email = User.Identity.Name;
+            var worker = await _context.Workers
+                .Include(x => x.Speciality)
+                    .ThenInclude(x => x.Department)
+                .Include(x => x.PreviousProductsWorkers)
+                    .ThenInclude(x => x.PreviousProduct)
+                .Include(x => x.FavoriteProductsWorkers)
+                    .ThenInclude(x => x.FavoriteProduct)
+                .SingleAsync(x => x.Email == email);
+
+            return Ok(ResponseProfile.FromApiProfile(worker,
+                context: _context,
+                avatar: _getAvatar(worker.AvatarUrl)
+            ));
+        }
+
+        //PUT: api/Worker/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutWorker(int id, Worker worker)
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutWorker(int id, Models.Worker worker)
+        //{
+        //    if (id != worker.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(worker).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!WorkerExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/Worker
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Worker>> PostWorker(Worker worker)
+        //{
+        //    _context.Workers.Add(worker);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetWorker", new { id = worker.Id }, worker);
+        //}
+
+        //// DELETE: api/Worker/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteWorker(int id)
+        //{
+        //    var worker = await _context.Workers.FindAsync(id);
+        //    if (worker == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Workers.Remove(worker);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool WorkerExists(int id)
+        //{
+        //    return _context.Workers.Any(e => e.Id == id);
+        //}
+
+        Models.File _getAvatar(string avatarUrl)
         {
-            if (id != worker.Id)
+            if (avatarUrl != null)
             {
-                return BadRequest();
+                var fullFileName = avatarUrl.Replace(Constans.ApiUrl + Constans.FileDownloadPart, string.Empty);
+                return _context.Files.SingleOrDefault(x => x.Name == fullFileName);
             }
-
-            _context.Entry(worker).State = EntityState.Modified;
-
-            try
+            else
             {
-                await _context.SaveChangesAsync();
+                return null;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!WorkerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Worker
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Worker>> PostWorker(Worker worker)
-        {
-            _context.Workers.Add(worker);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetWorker", new { id = worker.Id }, worker);
-        }
-
-        // DELETE: api/Worker/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteWorker(int id)
-        {
-            var worker = await _context.Workers.FindAsync(id);
-            if (worker == null)
-            {
-                return NotFound();
-            }
-
-            _context.Workers.Remove(worker);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool WorkerExists(int id)
-        {
-            return _context.Workers.Any(e => e.Id == id);
         }
     }
 }
---
+++ restcorporate_portal/ResponseModels/ResponseProfile.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using restcorporate_portal.Models;
+
+namespace restcorporate_portal.ResponseModels
+{
+    public class ResponseProfile
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public string FullName { get => $"{FirstName} {LastName}"; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public int Experience { get; set; }
+        public ResponseFileList Avatar { get; set; }
+        public int Balance { get; set; }
+        public virtual ResponseSpecialityList Speciality { get; set; }
+        public virtual List<ResponseProductList> PreviousProducts { get; set; }
+        public virtual List<ResponseProductList> FavoriteProducts { get; set; }
+
+        public static ResponseProfile FromApiProfile(Worker value, corporateContext context, File avatar = null)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return new ResponseProfile
+            {
+                Id = value.Id,
+                FirstName = value.FirstName,
+                LastName = value.LastName,
+                Email = value.Email,
+                Password = null,
+                Experience = value.Experience,
+                Balance = value.Balance,
+                Avatar = ResponseFileList.FromApiFile(avatar),
+                Speciality = ResponseSpecialityList.FromApiSpeciality(value.Speciality),
+                PreviousProducts = value.PreviousProductsWorkers
+                    .Select(x => {
+                        var image = context.Files.SingleOrDefault(y => x.PreviousProduct.ImageUrl.Contains(y.Name));
+                        var isFavorite = x.PreviousProduct.FavoriteProductsWorkers.Any(y => y.WorkerId == value.Id && y.FavoriteProductId == x.Id);
+                        return ResponseProductList.FromApiProduct(x.PreviousProduct,
+                            image: image,
+                            isFavorite: isFavorite,
+                            isCanBuy: value.Balance >= x.PreviousProduct.Price);
+                    }).ToList(),
+                FavoriteProducts = value.FavoriteProductsWorkers
+                    .Select(x => {
+                        var image = context.Files.SingleOrDefault(y => x.FavoriteProduct.ImageUrl.Contains(y.Name));
+                        return ResponseProductList.FromApiProduct(x.FavoriteProduct,
+                            image: image,
+                            isFavorite: true,
+                            isCanBuy: value.Balance >= x.FavoriteProduct.Price);
+                    }).ToList()
+            };
+        }
+    }
+}
