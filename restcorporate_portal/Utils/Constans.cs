6c8079564848790e4a0ad9fc09ad63afd598cde0
Top tinker 50/50 <tropin@mintmail.ru>
2021-03-27 03:24:17 +0500

    dd

--- restcorporate_portal/Utils/Constans.cs
+++ restcorporate_portal/Utils/Constans.cs
@@ -6,7 +6,7 @@ namespace restcorporate_portal.Utils
     public static class Constans
     {
         public const string FileDownloadPart = @"/api/files/download?filename=";
-        public const string ApiUrl = "https://localhost:5001";
+        public const string ApiUrl = "http://localhost:5001";
         public static string OSFilesPath(string rootPath) => rootPath +
                 (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ?
                 "/Upload/" :
