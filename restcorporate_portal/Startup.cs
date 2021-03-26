d0b3ed1b1aa4cf32f57a6034083e4d06cbb638ee
Top tinker 50/50 <tropin@mintmail.ru>
2021-03-27 03:23:43 +0500

    Remove https redirection

--- restcorporate_portal/Startup.cs
+++ restcorporate_portal/Startup.cs
@@ -145,7 +145,7 @@ namespace restcorporate_portal
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "restcorporate_portal v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
